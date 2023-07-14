e6e4db62401db72f6f54c7b3f6420a1ae78e9fce
n4inz <zulkar.n4inz@gmail.com>
2023-07-15 06:22:31 +0800

    sdas

--- function.php
+++ function.php
@@ -37,13 +37,16 @@ function videoToAudio(){
 
         // $command = "$ffmpegPath -loop 1 -i $imageFileEscaped -i $audioFileEscaped -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest $audioOutputPath";
         exec($command);
-        // Mengatur header untuk mengunduh file
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="' . basename($audioOutputPath) . '"');
-        header('Content-Length: ' . filesize($audioOutputPath));
-        readfile($audioOutputPath);
-        
+        if (file_exists($audioOutputPath)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="' . basename($audioOutputPath) . '"');
+            header('Content-Length: ' . filesize($audioOutputPath));
+            readfile($audioOutputPath);
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+        }else{
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+        }
     }catch(Exception $e){
         echo $e;
         return false;
